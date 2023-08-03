49b8bbfb3963a03514af241021632037fd762e8f
jalal-khan <jalalusafxai@gmail.com>
2023-08-03 15:15:39 +0500

    Add method for employee

--- Vidly/Controllers/EmployeesController.cs
+++ Vidly/Controllers/EmployeesController.cs
@@ -1,13 +1,52 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Vidly.Data;
+using Vidly.Models;
+using Vidly.Models.Domain;
 
 namespace Vidly.Controllers
 {
     public class EmployeesController : Controller
     {
+        private readonly VidlyDbContext DbContext;
+        //private readonly VidlyDbContext _DbContext;
+
+        public EmployeesController(VidlyDbContext dbContext)
+        {
+            this.DbContext = dbContext;
+            //_DbContext= dbContext;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var employees = await DbContext.Employees.ToListAsync();
+            return View(employees);
+        }
+
         [HttpGet]
         public IActionResult Add()
         {
-            return View();
+            return View(); 
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(AddEmployeeViewModel addEmployeeRequest)
+        {
+            var employee = new Employee()
+            {
+                Id=Guid.NewGuid(),
+                Name= addEmployeeRequest.Name,
+                Email=addEmployeeRequest.Email,
+                Salary=addEmployeeRequest.Salary,
+                DateOfBirth=addEmployeeRequest.DateOfBirth,
+                Department=addEmployeeRequest.Department,
+
+            };
+            await DbContext.Employees.AddAsync(employee);
+            await DbContext.SaveChangesAsync();
+            return RedirectToAction("Index");
+
         }
     }
 }
--- Vidly/Models/AddEmployeeViewModel.cs
+++ Vidly/Models/AddEmployeeViewModel.cs
@@ -1,12 +1,11 @@
-﻿namespace Vidly.Models.Domain
+﻿namespace Vidly.Models
 {
-    public class Employee
+    public class AddEmployeeViewModel
     {
-        public Guid Id { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
         public long Salary { get; set; }
-        public DateTime DateOfBirth { get; set;}
+        public DateTime DateOfBirth { get; set; }
         public string Department { get; set; }
     }
 }
